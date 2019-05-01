6a0312f7da8526eff0026d35ffcd3edb24bef4c7
evaizli <everrich2018@gmail.com>
2019-04-30 17:37:51 -0700

    add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
+    resource :session, only: [:create, :destory]
   end
 
   root "static_pages#root"
