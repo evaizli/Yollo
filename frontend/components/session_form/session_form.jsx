3a81f4977edd2f2f9f34b73fd85303676866f903
evaizli <everrich2018@gmail.com>
2019-05-01 16:54:37 -0700

    update .gitignore file

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -4,7 +4,6 @@ class SessionForm extends React.Component{
 
     constructor(props){
         super(props);
-        console.log(props);
         this.state = {
             name: "",
             email: "",
