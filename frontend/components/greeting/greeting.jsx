611f781eb91fa3fce39d6b63c19366293078805e
evaizli <everrich2018@gmail.com>
2019-05-02 16:31:15 -0700

    style splash page

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -4,19 +4,24 @@ import { Link } from "react-router-dom";
 
 const Greeting = ({ currentUser, logout }) => {
     const sessionLinks = () => (
-    <header className="fixed-up-nav">
-        <nav className="navBar">
-            <i className="logo fab fa-trello"></i>
-            <h2>Yollo</h2>
-            <div className="space"></div>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign Up</Link>
-        </nav>
-    </header>
+    <>
+        <header className="fixed-up-nav">
+            <nav className="navBar">
+                <i className="logo fab fa-trello"></i>
+                <h2>Yollo</h2>
+                <div className="space"></div>
+                <Link to="/login">Log In</Link>
+                <Link to="/signup"><span>Sign Up</span></Link>
+            </nav>
+        </header>
+        <section className="demo-section">
+                <h2>I'm trying 一生懸命cssを勉強します</h2>
+        </section>
+    </>
     );
     const personalGreeting = () => (
-    <header className="fixed-up-nav">
-        <nav className="navBar" >
+    <header className="login-nav">
+        <nav className="loginBar" >
             <h2 >Hi, {currentUser.name}!</h2>
             <button onClick={logout}>Log Out</button>
         </nav>
