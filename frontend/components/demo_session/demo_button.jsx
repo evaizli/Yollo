337bf411cd5e89c622e78b4ccf24dfde4a152385
evaizli <everrich2018@gmail.com>
2019-05-03 09:52:08 -0700

    update landing css

--- frontend/components/demo_session/demo_button.jsx
+++ frontend/components/demo_session/demo_button.jsx
@@ -15,18 +15,21 @@ class DemoButton extends React.Component{
     render(){
         return(
             <div className="outter-demo-container">
-            <div className="demo-user-container">
-                <h1 className="tag-line-message">
-                    Yollo lets you work more collaboratively and get more done. 
-                </h1>
-                <p className="sub-tag-line-message">
-                    Yollo’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible, and rewarding way.
-                </p>
-                <input type="submit-demo-user" type="submit" value="Demo Log In" onClick={this.submitDemoUser}/>
-                <img className="hero-image-demo" src="hero-a.svg" alt=""/>
-
+            <div className="demo-user-container-center">
+                <div className="left-text-div">
+                    <h1 className="tag-line-message">
+                        Yollo lets you work more collaboratively and get more done. 
+                    </h1>
+                    <p className="sub-tag-line-message">
+                        Yollo’s boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible, and rewarding way.
+                    </p>
+                    <input type="submit-demo-user" type="submit" value="Demo Log In" onClick={this.submitDemoUser}/>
+                </div>
+                <div className ="right-image">
+                    <img className="hero-image-demo" src="hero-a.svg" alt=""/>
+                </div>
             </div>
-            </div>
+        </div>
         );
     }
 
