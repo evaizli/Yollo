37be5a44903adc8375d7b3a2aee8c3f4d14e33e8
evaizli <everrich2018@gmail.com>
2019-05-06 17:45:58 -0700

    add list and card seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 User.destroy_all
 Board.destroy_all
 BoardShare.destroy_all
+List.destroy_all
 
 user1=User.create(name: "testing", email:"testing@testing.com", password: "password")
 user2=User.create(name: "Alexander Hamilton", email:"alex@broadway.com", password: "password")
@@ -28,3 +29,19 @@ boardShare3=BoardShare.create(board_id: board3.id, user_id: user2.id)
 boardShare4=BoardShare.create(board_id: board4.id, user_id: user3.id)
 boardShare5=BoardShare.create(board_id: board5.id, user_id: user4.id)
 boardShare6=BoardShare.create(board_id: board3.id, user_id: user1.id)
+
+
+list1=List.create(title:"User Auth MVP1", board_id: board1.id)
+list2=List.create(title:"Boards MVP2", board_id: board1.id)
+list3=List.create(title:"Lists & Cards MVP3", board_id: board1.id)
+list4=List.create(title:"Sharing Boards MVP4", board_id: board1.id)
+list5=List.create(title:"Comments & Due Dates MVP5", board_id: board1.id)
+
+card1=Card.create(title:"resolve gitbut case", description:"check github for notes", list_id: list1.id)
+card2=Card.create(title:"Add CSS on Splash Page", description:"add footer", list_id: list1.id)
+card3=Card.create(title:"Backend Code & Routes", description:"code & test backend", list_id: list2.id)
+card4=Card.create(title:"Frontend Code & Routes", description:"code & test frontend", list_id: list2.id)
+card5=Card.create(title:"Backend Code & Routes", description:"code & test backend", list_id: list3.id)
+card6=Card.create(title:"Frontend Code & Routes", description:"code & test frontend", list_id: list3.id)
+card7=Card.create(title:"Backend Code & Routes", description:"code & test backend", list_id: list4.id)
+card8=Card.create(title:"Frontend Code & Routes", description:"code & test frontend", list_id: list4.id)
